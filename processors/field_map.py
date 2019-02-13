dc4e5fb35d815adfc21537d65820782c2a65e6ec
Andi és Bali <bszabomisc@gmail.com>
2019-02-13 11:31:14 +0100

    Fixed phone num pickup

--- processors/field_map.py
+++ processors/field_map.py
@@ -67,6 +67,7 @@ USABLE_FIELDS = [
     ('Kapcsolattarto', Fields.PHONE1),
     ('Kapcsolattarto telefon', Fields.PHONE1),
     ('Ertesitesi telefon', Fields.PHONE1),
+    ('Ertesitesi telefon (Mobil)', Fields.PHONE1),
     ('Ertesitesi telefon (Otthoni)', Fields.PHONE2),
     ('Telefonszam', Fields.PHONE2),
 
